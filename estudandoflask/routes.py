b622b81c2d2eb2b6da62b9d8505f94de149b880b
José Nilton Vieira Lima Filho <jose.lima@COMETA.INTRANET>
2022-07-29 17:37:50 -0300

    Criando funcoes de Editar Post e Excluir Post

--- estudandoflask/routes.py
+++ estudandoflask/routes.py
@@ -115,7 +115,22 @@ def editar_perfil():
     return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
 
 
-@app.route('/post/<post_id>')
+@app.route('/post/<post_id>', methods=['GET','POST'])
+@login_required
 def exibir_post(post_id):
     post = Post.query.get(post_id)
-    return render_template('post.html', post=post)
+    if current_user == post.autor:
+        form = FormCriarPost()
+        if request.method == 'GET':
+            form.titulo.data = post.titulo
+            form.corpo.data = post.corpo
+        #Lógica de editar Post
+        elif form.validate_on_submit():
+            post.titulo = form.titulo.data
+            post.corpo = form.corpo.data
+            database.session.commit()
+            flash('Post atualizado com sucesso!', 'alert-success')
+            return redirect(url_for('home'))
+    else:
+        form = None
+    return render_template('post.html', post=post, form=form)
